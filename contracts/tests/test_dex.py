669bfb3424fe4b1a923e01a6b52a7c0aa607640b
Dayitva <dayitvagoel@gmail.com>
2022-03-25 22:30:47 +0530

    fixed some tests

--- contracts/tests/test_dex.py
+++ contracts/tests/test_dex.py
@@ -14,9 +14,9 @@ def test():
     oscar = sp.address("tz1-Oscar")
     token_admin = sp.address("tz1-TokenAdmin")
 
-    DECIMALS_0 = 10 ** 8
-    DECIMALS_1 = 10 ** 6
-    DECIMALS_E = 10 ** 18
+    DECIMALS_USDTZ = 10 ** 6
+    DECIMALS_KUSD = 10 ** 8
+    DECIMALS_EIGHTEEN = 10 ** 18
 
     scenario = sp.test_scenario()
 
@@ -67,28 +67,29 @@ def test():
 
     kusd.mint(
         address=alice,
-        value=sp.nat(100000 * DECIMALS_1)
+        value=sp.nat(100000 * DECIMALS_KUSD)
     ).run(sender=token_admin)
     kusd.mint(
         address=admin,
-        value=sp.nat(100000 * DECIMALS_1)
+        value=sp.nat(100000 * DECIMALS_KUSD)
     ).run(sender=token_admin)
     kusd.mint(
         address=bob,
-        value=sp.nat(100000 * DECIMALS_1)
+        value=sp.nat(100000 * DECIMALS_KUSD)
     ).run(sender=token_admin)
     usdtz.mint(
         address=alice,
-        value=sp.nat(100000 * DECIMALS_0)
+        value=sp.nat(100000 * DECIMALS_USDTZ)
     ).run(sender=token_admin)
     usdtz.mint(
         address=admin,
-        value=sp.nat(100000 * DECIMALS_0)
+        value=sp.nat(100000 * DECIMALS_USDTZ)
     ).run(sender=token_admin)
     usdtz.mint(
         address=bob,
-        value=sp.nat(100000 * DECIMALS_0)
+        value=sp.nat(100000 * DECIMALS_USDTZ)
     ).run(sender=token_admin)
+    
     lp_metadata = {
         "decimals": "9",
         "name": "LP Token",
@@ -112,8 +113,8 @@ def test():
         # y_address=sp.address('KT1CNQL6xRn5JaTUcMmxwSc5YQjwpyHkDR5r'),
         _lp_token=lp.address,
         _admin=admin,
-        x_decimals=sp.nat(DECIMALS_1),
-        y_decimals=sp.nat(DECIMALS_0),
+        x_decimals=sp.nat(DECIMALS_KUSD),
+        y_decimals=sp.nat(DECIMALS_USDTZ),
         x_is_fa2=False,
         y_is_fa2=False,
         x_token_id=0,
@@ -132,24 +133,24 @@ def test():
 
     kusd.approve(sp.record(
         spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_0
+        value=sp.nat(50_000 * DECIMALS_KUSD
                      ))).run(sender=admin)
     usdtz.approve(sp.record(
         spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_1
+        value=sp.nat(50_000 * DECIMALS_USDTZ
                      ))).run(sender=admin)
 
     scenario.h2("[INVALID] Initialize exchange.")
     dex.initialize_exchange(
-        token1_amount=sp.nat(50_000 * DECIMALS_1),
-        token2_amount=sp.nat(50_000 * DECIMALS_0)
+        token1_amount=sp.nat(50_000 * DECIMALS_KUSD),
+        token2_amount=sp.nat(50_000 * DECIMALS_USDTZ)
     ).run(sender=alice, valid=False)
 
-    # scenario.h2("Initialize exchange.")
-    # dex.initialize_exchange(
-    #     token1_amount=sp.nat(50_000 * DECIMALS_1),
-    #     token2_amount=sp.nat(50_000 * DECIMALS_0)
-    # ).run(sender=admin)
+    scenario.h2("Initialize exchange.")
+    dex.initialize_exchange(
+        token1_amount=sp.nat(50_000 * DECIMALS_KUSD),
+        token2_amount=sp.nat(50_000 * DECIMALS_USDTZ)
+    ).run(sender=admin)
 
     # scenario.h2("[INVALID] Remove liquidity.")
     # dex.remove_liquidity(_amount=25000 * (10**18), min_tokens={
@@ -158,160 +159,160 @@ def test():
     # dex.remove_liquidity(_amount=25000 * (10**18), min_tokens={
     #                      0: sp.as_nat(1), 1: sp.as_nat(1)}).run(sender=oscar, valid=False)
 
-    usdtz.approve(sp.record(
-        spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_1)
-    )).run(sender=bob)
     kusd.approve(sp.record(
         spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_0)
+        value=sp.nat(50_000 * DECIMALS_KUSD)
     )).run(sender=bob)
-
+    usdtz.approve(sp.record(
+        spender=dex.address,
+        value=sp.nat(50_000 * DECIMALS_USDTZ)
+    )).run(sender=bob)
+    
     scenario.h2("Exchange tokens")
-    dex.exchange(i=0, dx=5000 * DECIMALS_1, min_dy=1,
+    dex.exchange(i=1, dx=5000 * DECIMALS_USDTZ, min_dy=1 * DECIMALS_KUSD,
                  time_valid_upto=sp.timestamp(100000)).run(sender=bob)
 
     kusd.approve(sp.record(
         spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_0
+        value=sp.nat(50_000 * DECIMALS_KUSD
                      ))).run(sender=alice)
 
     usdtz.approve(sp.record(
         spender=dex.address,
-        value=sp.nat(50_000 * DECIMALS_1
+        value=sp.nat(50_000 * DECIMALS_USDTZ
                      ))).run(sender=alice)
 
     ### TESTS BEGIN HERE ###
 
     scenario.h2("Test 1: Initialising by Oscar. Should fail as he is not admin.")
     dex.initialize_exchange(
-        token1_amount=sp.nat(50_000 * DECIMALS_0),
-        token2_amount=sp.nat(50_000 * DECIMALS_1)
+        token1_amount=sp.nat(50_000 * DECIMALS_KUSD),
+        token2_amount=sp.nat(50_000 * DECIMALS_USDTZ)
     ).run(sender=oscar, valid=False)
 
     scenario.h2("Alice :: Adding liquidity. 200")
-    dex.add_liquidity(_amount0=200 * DECIMALS_1, _amount1=0 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=200 * DECIMALS_USDTZ, _amount1=0 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
     scenario.h2("Alice :: Adding liquidity. 10")
-    dex.add_liquidity(_amount0=10 * DECIMALS_1, _amount1=0 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=10 * DECIMALS_USDTZ, _amount1=0 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
     scenario.h2("Alice :: Adding liquidity. 5000")
-    dex.add_liquidity(_amount0=5000 * DECIMALS_1, _amount1=0 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=5000 * DECIMALS_USDTZ, _amount1=0 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
     scenario.h2("Alice :: Adding liquidity. 200")
-    dex.add_liquidity(_amount0=200 * DECIMALS_1, _amount1=200 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=200 * DECIMALS_USDTZ, _amount1=200 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
     scenario.h2("Alice :: Adding liquidity. 100")
-    dex.add_liquidity(_amount0=100 * DECIMALS_1, _amount1=10 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=100 * DECIMALS_USDTZ, _amount1=10 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
     scenario.h2("Alice :: Adding liquidity. 5000")
-    dex.add_liquidity(_amount0=5000 * DECIMALS_1, _amount1=100 *
-                      DECIMALS_0, min_token=1).run(sender=alice)
+    dex.add_liquidity(_amount0=5000 * DECIMALS_USDTZ, _amount1=100 *
+                      DECIMALS_KUSD, min_token=1).run(sender=alice)
 
-    dex.remove_liquidity(_amount=200 * DECIMALS_1,
+    dex.remove_liquidity(_amount=200 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
-    dex.remove_liquidity(_amount=10 * DECIMALS_1,
+    dex.remove_liquidity(_amount=10 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
-    dex.remove_liquidity(_amount=5000 * DECIMALS_1,
+    dex.remove_liquidity(_amount=5000 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
-    dex.remove_liquidity(_amount=400 * DECIMALS_1,
+    dex.remove_liquidity(_amount=400 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
-    dex.remove_liquidity(_amount=110 * DECIMALS_1,
+    dex.remove_liquidity(_amount=110 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
-    dex.remove_liquidity(_amount=4500 * DECIMALS_1,
+    dex.remove_liquidity(_amount=4500 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=alice)
 
-    dex.remove_liquidity(_amount=50_000 * DECIMALS_1,
+    dex.remove_liquidity(_amount=50_000 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=admin)
-    dex.remove_liquidity(_amount=50_000 * DECIMALS_1,
+    dex.remove_liquidity(_amount=50_000 * DECIMALS_USDTZ,
                          min_tokens={0: 1, 1: 1}).run(sender=admin)
 
     scenario.h2(
         "Test 5: Adding liquidity by Alice. Should fail as she is asking for more min tokens than possible.")
-    dex.add_liquidity(_amount0=200 * DECIMALS_0, _amount1=0 *
-                      DECIMALS_1, min_token=200).run(sender=alice, valid=False)
+    dex.add_liquidity(_amount0=200 * DECIMALS_KUSD, _amount1=0 *
+                      DECIMALS_USDTZ, min_token=200).run(sender=alice, valid=False)
 
     scenario.h2(
         "Test 6: Adding liquidity by Alice. Should fail as she is adding more liquidity than what she is approved for.")
-    dex.add_liquidity(_amount0=100001 * DECIMALS_0, _amount1=200 *
-                      DECIMALS_1, min_token=400).run(sender=alice, valid=False)
+    dex.add_liquidity(_amount0=100001 * DECIMALS_KUSD, _amount1=200 *
+                      DECIMALS_USDTZ, min_token=400).run(sender=alice, valid=False)
 
     scenario.h2(
         "Test 7: Adding liquidity by Alice. Should fail as she is sending tez along with the transaction.")
-    dex.add_liquidity(_amount0=200 * DECIMALS_0, _amount1=0 * DECIMALS_1,
+    dex.add_liquidity(_amount0=200 * DECIMALS_KUSD, _amount1=0 * DECIMALS_USDTZ,
                       min_token=200).run(sender=alice, amount=sp.mutez(1), valid=False)
 
     scenario.h2(
         "Test 8: Adding liquidity by Alice. Should succeed with correct return values.")
-    dex.add_liquidity(_amount0=200 * DECIMALS_0, _amount1=200 *
-                      DECIMALS_1, min_token=400).run(sender=alice)
-    scenario.verify(dex.data.token_pool[0].pool == 50200 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool == 50200 * DECIMALS_E)
-    # scenario.verify(dex.data.lp_balance[alice.address] == 400 * DECIMALS_E)
+    dex.add_liquidity(_amount0=200 * DECIMALS_KUSD, _amount1=200 *
+                      DECIMALS_USDTZ, min_token=400).run(sender=alice)
+    scenario.verify(dex.data.token_pool[0].pool == 50200 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool == 50200 * DECIMALS_EIGHTEEN)
+    # scenario.verify(dex.data.lp_balance[alice.address] == 400 * DECIMALS_EIGHTEEN)
 
     scenario.h2(
         "Test 9: Removing liquidity by Alice. Should fail as she is asking for more min tokens than possible.")
-    dex.remove_liquidity(_amount=400 * DECIMALS_E, min_tokens={
-                         0: 1000*DECIMALS_0, 1: 1000*DECIMALS_1}).run(sender=alice, valid=False)
+    dex.remove_liquidity(_amount=400 * DECIMALS_EIGHTEEN, min_tokens={
+                         0: 1000*DECIMALS_KUSD, 1: 1000*DECIMALS_USDTZ}).run(sender=alice, valid=False)
 
     scenario.h2(
         "Test 10: Removing liquidity by Alice. Should fail as she does not have sufficient LP tokens.")
-    dex.remove_liquidity(_amount=401 * DECIMALS_E, min_tokens={
-                         0: 200*DECIMALS_0, 1: 200*DECIMALS_1}).run(sender=alice, valid=False)
+    dex.remove_liquidity(_amount=401 * DECIMALS_EIGHTEEN, min_tokens={
+                         0: 200*DECIMALS_KUSD, 1: 200*DECIMALS_USDTZ}).run(sender=alice, valid=False)
 
     scenario.h2(
         "Test 11: Removing liquidity by Alice. Should fail as she is sending tez along with the transaction.")
-    dex.remove_liquidity(_amount=100 * DECIMALS_E, min_tokens={
-                         0: 2*DECIMALS_0, 1: 2*DECIMALS_1}).run(sender=alice, amount=sp.mutez(1), valid=False)
+    dex.remove_liquidity(_amount=100 * DECIMALS_EIGHTEEN, min_tokens={
+                         0: 2*DECIMALS_KUSD, 1: 2*DECIMALS_USDTZ}).run(sender=alice, amount=sp.mutez(1), valid=False)
 
     scenario.h2(
         "Test 12: Removing liquidity by Alice. Should succeed with correct return values.")
-    dex.remove_liquidity(_amount=400 * DECIMALS_E,
-                         min_tokens={0: 200*DECIMALS_0, 1: 200*DECIMALS_1}).run(sender=alice)
-    scenario.verify(dex.data.token_pool[0].pool == 50000 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool == 50000 * DECIMALS_E)
-    # scenario.verify(dex.data.lp_balance[alice.address] == 0 * DECIMALS_E)
+    dex.remove_liquidity(_amount=400 * DECIMALS_EIGHTEEN,
+                         min_tokens={0: 200*DECIMALS_KUSD, 1: 200*DECIMALS_USDTZ}).run(sender=alice)
+    scenario.verify(dex.data.token_pool[0].pool == 50000 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool == 50000 * DECIMALS_EIGHTEEN)
+    # scenario.verify(dex.data.lp_balance[alice.address] == 0 * DECIMALS_EIGHTEEN)
 
     scenario.h2(
         "Test 13: Exchange operation by Bob. Should fail as he is asking for more min tokens than allowed.")
-    dex.exchange(i=0, dx=5000 * DECIMALS_0, min_dy=5001 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(2022,
+    dex.exchange(i=0, dx=5000 * DECIMALS_KUSD, min_dy=5001 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(2022,
                  2, 21, 23, 59, 59)).run(sender=bob, valid=False, now=sp.timestamp_from_utc(2022, 2, 20, 23, 59, 59))
 
     scenario.h2(
         "Test 14: Exchange operation by Bob. Should fail as he is exchanging more tokens than he is approved for.")
-    dex.exchange(i=0, dx=100001 * DECIMALS_0, min_dy=5001 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(
+    dex.exchange(i=0, dx=100001 * DECIMALS_KUSD, min_dy=5001 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(
         2022, 2, 21, 23, 59, 59)).run(sender=bob, valid=False, now=sp.timestamp_from_utc(2022, 2, 20, 23, 59, 59))
 
     scenario.h2(
         "Test 15: Exchange operation by Bob. Should fail as the transaction takes more than he wanted it valid for.")
-    dex.exchange(i=0, dx=100001 * DECIMALS_0, min_dy=5001 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(
+    dex.exchange(i=0, dx=100001 * DECIMALS_KUSD, min_dy=5001 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(
         2022, 2, 21, 23, 59, 59)).run(sender=bob, valid=False, now=sp.timestamp_from_utc(2022, 2, 22, 23, 59, 59))
 
     scenario.h2(
         "Test 16: Exchange operation by Bob. Should fail as he is sending tez along with the transaction.")
-    dex.exchange(i=0, dx=100 * DECIMALS_0, min_dy=50 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(2022, 2, 21,
+    dex.exchange(i=0, dx=100 * DECIMALS_KUSD, min_dy=50 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(2022, 2, 21,
                  23, 59, 59)).run(sender=bob, amount=sp.mutez(1), valid=False, now=sp.timestamp_from_utc(2022, 2, 22, 23, 59, 59))
 
     scenario.h2(
         "Test 17: Exchange operation by Bob. Should succeed with correct return values.")
-    dex.exchange(i=0, dx=5000 * DECIMALS_0, min_dy=4986 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(
+    dex.exchange(i=0, dx=5000 * DECIMALS_KUSD, min_dy=4986 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(
         2022, 2, 21, 23, 59, 59)).run(sender=bob, now=sp.timestamp_from_utc(2022, 2, 20, 23, 59, 59))
-    scenario.verify(dex.data.token_pool[0].pool == 55000 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool >= 45213 * DECIMALS_0)
+    scenario.verify(dex.data.token_pool[0].pool == 55000 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool >= 45213 * DECIMALS_KUSD)
 
     scenario.h2(
         "Test 18: Exchange operation by Bob from token 0 to token 1. Should succeed with correct return values.")
-    dex.exchange(i=0, dx=1000 * DECIMALS_0, min_dy=995 * DECIMALS_1, time_valid_upto=sp.timestamp_from_utc(
+    dex.exchange(i=0, dx=1000 * DECIMALS_KUSD, min_dy=995 * DECIMALS_USDTZ, time_valid_upto=sp.timestamp_from_utc(
         2022, 2, 21, 23, 59, 59)).run(sender=bob, now=sp.timestamp_from_utc(2022, 2, 20, 23, 59, 59))
-    scenario.verify(dex.data.token_pool[0].pool == 56000 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool >= 44017 * DECIMALS_0)
+    scenario.verify(dex.data.token_pool[0].pool == 56000 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool >= 44017 * DECIMALS_KUSD)
 
     scenario.h2(
         "Test 19: Exchange operation by Bob from token 1 to token 0. Should succeed with correct return values.")
-    dex.exchange(i=1, dx=1000 * DECIMALS_1, min_dy=1001 * DECIMALS_0, time_valid_upto=sp.timestamp_from_utc(
+    dex.exchange(i=1, dx=1000 * DECIMALS_USDTZ, min_dy=1001 * DECIMALS_KUSD, time_valid_upto=sp.timestamp_from_utc(
         2022, 2, 21, 23, 59, 59)).run(sender=bob, now=sp.timestamp_from_utc(2022, 2, 20, 23, 59, 59))
-    scenario.verify(dex.data.token_pool[0].pool >= 54998 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool >= 45017 * DECIMALS_0)
+    scenario.verify(dex.data.token_pool[0].pool >= 54998 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool >= 45017 * DECIMALS_KUSD)
 
     scenario.h2(
         "Test 20: Claiming admin fees by Oscar. Should fail as he is not admin.")
@@ -319,20 +320,20 @@ def test():
 
     scenario.h2("Test 21: Claiming admin fees by Admin. Should succeed.")
     dex.admin_claim().run(sender=admin)
-    scenario.verify(dex.data.token_pool[0].pool >= 54998 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool >= 45012 * DECIMALS_E)
+    scenario.verify(dex.data.token_pool[0].pool >= 54998 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool >= 45012 * DECIMALS_EIGHTEEN)
 
     scenario.h2(
         "Test 22: Removing all liquidity by Admin. Should fail as admin is removing more liquidity than allowed for.")
-    dex.remove_liquidity(_amount=100001 * DECIMALS_E, min_tokens={
-                         0: 50000*DECIMALS_0, 1: 50000*DECIMALS_1}).run(sender=admin, valid=False)
+    dex.remove_liquidity(_amount=100001 * DECIMALS_EIGHTEEN, min_tokens={
+                         0: 50000*DECIMALS_KUSD, 1: 50000*DECIMALS_USDTZ}).run(sender=admin, valid=False)
 
     scenario.h2("Test 23: Removing all liquidity by Admin. Should succeed.")
-    dex.remove_liquidity(_amount=100000 * DECIMALS_E,
-                         min_tokens={0: 40000*DECIMALS_0, 1: 40000*DECIMALS_1}).run(sender=admin)
-    scenario.verify(dex.data.token_pool[0].pool <= 10 * DECIMALS_E)
-    scenario.verify(dex.data.token_pool[1].pool <= 10 * DECIMALS_E)
-    # scenario.verify(dex.data.lp_balance[admin.address] == 0 * DECIMALS_E)
+    dex.remove_liquidity(_amount=100000 * DECIMALS_EIGHTEEN,
+                         min_tokens={0: 40000*DECIMALS_KUSD, 1: 40000*DECIMALS_USDTZ}).run(sender=admin)
+    scenario.verify(dex.data.token_pool[0].pool <= 10 * DECIMALS_EIGHTEEN)
+    scenario.verify(dex.data.token_pool[1].pool <= 10 * DECIMALS_EIGHTEEN)
+    # scenario.verify(dex.data.lp_balance[admin.address] == 0 * DECIMALS_EIGHTEEN)
 
     scenario.h2("Test 24: Updating A by Oscar. Should fail as he is not admin.")
     dex.update_A(sp.nat(100)).run(sender=oscar, valid=False)
@@ -359,5 +360,5 @@ def test():
     # scenario.h2(
     #     "Test 30: Adding liquidity by Alice. Should fail as the the contract is paused by the admin.")
     # dex.togglePause().run(sender=admin)
-    # dex.add_liquidity(_amount0=10 * DECIMALS_0, _amount1=20 *
-    #                   DECIMALS_1, min_token=1).run(sender=alice, valid=False)
+    # dex.add_liquidity(_amount0=10 * DECIMALS_KUSD, _amount1=20 *
+    #                   DECIMALS_USDTZ, min_token=1).run(sender=alice, valid=False)
